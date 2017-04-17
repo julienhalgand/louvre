1267ca638b1637811d13b03ca9c93d9846aecc66
Julien Halgand <julien.halgand1@gmail.com>
2017-04-17 19:17:32 +0200

    Formulaire étape 1 coté client OK

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/step1", name="step1")
+     */
+    public function step1Action(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/step1.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
 }
